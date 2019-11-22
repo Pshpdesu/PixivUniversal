9e6043facf518463d4a17ee33f09c46146d5a75e
Pavel Shipulin <pshipulin@gmcs.ru>
2019-11-22 23:56:20 +0300

    Check tokens lifetime

--- RePixivAPI/PixivAPI.cs
+++ RePixivAPI/PixivAPI.cs
@@ -5,14 +5,32 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using static RePixivAPI.Helpers.AuthenticationConstants;
 
 namespace RePixivAPI
 {
     public class Tokens
     {
+        [JsonProperty, JsonRequired]
         public string RefreshToken { get; set; }
+        [JsonProperty, JsonRequired]
         public string AccessToken { get; set; }
+        [JsonProperty(PropertyName = "ExpiresIn")]
+        private int _ExpiresIn;
+        [JsonIgnore]
+        public int ExpiresIn
+        {
+            get
+            {
+                return _ExpiresIn;
+            }
+            set
+            {
+                ExpirationTime = DateTime.UtcNow.AddSeconds(value);
+                _ExpiresIn = value;
+            }
+        }
         public DateTime ExpirationTime { get; set; }
     }
 
@@ -31,17 +49,22 @@ namespace RePixivAPI
             await client.Authorize(grantType, credentials);
             return client;
         }
-        
+
 
         Credentials userCredentials { get; set; }
 
         private PixivAPIClient()
-        {}
-
+        { }
 
         private async Task Authorize(GrantType grantType, Credentials credentials)
         {
-            userCredentials = credentials;
+            this.userCredentials = credentials;
+            await Authorize(grantType);
+        }
+
+        private async Task Authorize(GrantType grantType)
+        {
+            //userCredentials = credentials;
             var client = GetDefaultAuthHttpClient();
             var urlData = new FormUrlEncodedContent(GetAuthorizeContent(grantType, this.userCredentials));
             var test = await urlData.ReadAsStringAsync();
@@ -54,11 +77,18 @@ namespace RePixivAPI
         {
             var httpClient = new HttpClient();
             var headers = GetAuthHeaders();
-            foreach(var header in headers)
+            foreach (var header in headers)
             {
                 httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
             }
             return httpClient;
+        }
+        private  async Task CheckAuthorizationAsync()
+        {
+            if (userCredentials.Tokens.ExpirationTime >= DateTime.UtcNow)
+            {
+                await Authorize(GrantType.RefreshToken);
+            }
         }
 
         //public class AuthResult
